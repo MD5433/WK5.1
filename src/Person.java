5867970ddf61f4f859e0e661da38431c7683926c
MD5433 <mariahdungey01@gmail.com>
2025-02-16 23:53:12 -0500

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,40 +1,38 @@
- public class Person {
-        private String name;
-        private String email;
+public class Person {
+    private String name;
+    private String email;
 
-     public Person(String name, String email) {
-         this.name = name;
-         this.email = email;
-     }
+    public Person(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
 
-     public String getName() {
-         return name;
-     }
+    public String getName() {
+        return name;
+    }
 
-     public void setName(String name) {
-         this.name = name;
-     }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-     public String getEmail() {
-         return email;
-     }
+    public String getEmail() {
+        return email;
+    }
 
-     public void setEmail(String email) {
-         this.email = email;
-     }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-     @Override
-     public String toString() {
-         return "Person{" +
-                 "name='" + name + '\'' +
-                 ", email='" + email + '\'' +
-                 '}';
-     }
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 
 
 
 
 
 }
-
-

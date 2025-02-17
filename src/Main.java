932fd31c43e524b8f0940fe15e2a5e6dee3cc7c3
MD5433 <mariahdungey01@gmail.com>
2025-02-16 23:51:50 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
-import java.util.ArrayList;
+//Mariah Dungey 02/15/2025
 import java.util.Scanner;
 
 public class Main {
     static Scanner userIn = new Scanner(System.in);
-    static ArrayList usersInfo = new ArrayList<>();
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         enterInfo();
@@ -11,19 +11,28 @@ public class Main {
 
     }
 
-    static void enterInfo(){
-
-        while (!userIn.equals("end")){
-            System.out.println("Enter your first and last name.");
+    static void enterInfo() {
+        while (true) {
+            System.out.println("Enter your name, type end to stop");
             String usName = userIn.nextLine();
-            System.out.println("Enter your email.");
+            if (usName.equals("end")) {
+                break;
+            }
+
+            System.out.println("Enter your email:");
             String usEmail = userIn.nextLine();
 
+            Person person = new Person(usName, usEmail);
+            AddressBook.addContact(person);
 
-
+            System.out.println("Would you like to search for a contact by name? (yes/no)");
+            String searchResponse = userIn.nextLine();
+            if (searchResponse.equals("yes")) {
+                System.out.println("Enter the name of the contact you wish to search for:");
+                String search = userIn.nextLine();
+                AddressBook.searchContact(search);
+            }
         }
-
         userIn.close();
-
     }
 }
